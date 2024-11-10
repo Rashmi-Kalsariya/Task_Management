bb35599173bb9bb3dc04c30314c0cfc334b1a549
Rashmi-Kalsariya <rashmikalsariya02@gmail.com>
2024-11-10 12:27:41 +0530

    CLIENT

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from  "axios";
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:8090/api',
+    baseURL: 'https://task-management-server-2tup.onrender.com/api',
     headers: {
       'Content-Type': 'application/json',
     },
